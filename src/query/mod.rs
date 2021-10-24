17d2ea86f7b2fb0664bdd1f26f4385c8b25622fa
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-10-24 22:32:49 +0900

    add tests to query module

--- src/query/mod.rs
+++ src/query/mod.rs
@@ -85,3 +85,65 @@ fn assemble_single_query_param<T: ToString>(
 fn make_elem<T: ToString>(key: &str, value: T) -> (String, String) {
     (key.to_string(), value.to_string())
 }
+
+#[cfg(test)]
+mod test {
+    use crate::query::{assemble_query_param, assemble_single_query_param, make_elem};
+
+    use super::{builder::QueryBuilder, types::OrderOption};
+
+    #[test]
+    fn test_assemble_query_param() {
+        let mut queries = vec![("event_id".to_string(), "1".to_string())];
+        assemble_query_param(&mut queries, Some(vec![2, 3]), "event_id");
+        assert_eq!(queries.len(), 3);
+
+        let mut queries = vec![("event_id".to_string(), "1".to_string())];
+        assemble_query_param::<u8>(&mut queries, None, "event_id");
+        assert_eq!(queries.len(), 1);
+    }
+
+    #[test]
+    fn test_assemble_single_query_param() {
+        let mut queries = vec![("event_id".to_string(), "1".to_string())];
+        assemble_single_query_param(&mut queries, Some(2), "event_id");
+        assert_eq!(queries.len(), 2);
+
+        let mut queries = vec![("event_id".to_string(), "1".to_string())];
+        assemble_single_query_param::<u8>(&mut queries, None, "event_id");
+        assert_eq!(queries.len(), 1);
+    }
+
+    #[test]
+    fn test_make_elem() {
+        let (key, value) = make_elem("key", 1);
+        assert_eq!(key, "key".to_string());
+        assert_eq!(value, "1".to_string());
+    }
+
+    #[test]
+    fn test_make_reqwest_query_partially() {
+        let builder = QueryBuilder::begin()
+            .event_ids(vec![1, 2, 3])
+            .keyword("Rust".to_string())
+            .start(1)
+            .order(OrderOption::Newer)
+            .count(10)
+            .format("json".to_string());
+        let query = builder.build().unwrap();
+        let reqwest_query = query.make_reqwest_query();
+        assert_eq!(
+            reqwest_query,
+            vec![
+                make_elem("event_id", 1),
+                make_elem("event_id", 2),
+                make_elem("event_id", 3),
+                make_elem("keyword", "Rust"),
+                make_elem("start", 1),
+                make_elem("order", OrderOption::Newer.to_u8()),
+                make_elem("count", 10),
+                make_elem("format", "json")
+            ]
+        );
+    }
+}
